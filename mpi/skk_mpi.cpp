fa0b34faff0481de68685a971746efaea9241a23
armagumen <armagumen@gmail.com>
2014-05-08 19:04:36 -0400

    added the 10x18 test case

--- mpi/skk_mpi.cpp
+++ mpi/skk_mpi.cpp
@@ -47,7 +47,7 @@ static int height = _BHEIGHT;
 static int pp = 6;
 */
 
-
+/*
 #define _BWIDTH 10
 #define _BHEIGHT 10
 static int board[10][10] = {{0}};
@@ -58,6 +58,17 @@ static int yval[] = {0,9,4,5,1,8,3,6,3,6,1,8,4,5,0,9};
 static int width = 10;
 static int height = 10;
 static int pp = 16;
+*/
+#define _BWIDTH 10
+#define _BHEIGHT 18
+static int board[10][18] = {{0}};
+static int cellboard[10][18] = {{0}};
+static int pval[] = {4,9,6,4,6,9,6,4,6,4,4,2,4,4,8,3,8,4,6,4,3,9,6,4,3,4,6,6,6,4,4,4,2,6,2,6};
+static int xval[] = {0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9};
+static int yval[] = {0,6,8,16,7,9,14,17,1,3,12,15,2,6,13,1,7,9,8,10,16,4,11,15,2,5,14,16,0,3,8,10,1,9,11,17};
+static int width = 10;
+static int height = 18;
+static int pp = 36;
 
 
 
