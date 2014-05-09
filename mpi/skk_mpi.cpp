56c3eb09fd618d04c7433a1c1d1faba617fdf252
armagumen <aaguilet@andrew.cmu.edu>
2014-05-08 23:01:12 -0400

    started improving the mpi version

--- mpi/skk_mpi.cpp
+++ mpi/skk_mpi.cpp
@@ -58,6 +58,10 @@ static int yval[] = {0,9,4,5,1,8,3,6,3,6,1,8,4,5,0,9};
 static int width = 10;
 static int height = 10;
 static int pp = 16;
+static vector<int> changes_x;
+static vector<int> changes_y;
+static vector<int> changes_id;
+
 
 /*
 #define _BWIDTH 10
@@ -157,6 +161,9 @@ void fillUp(int num, rectangle r){
 	for (int j = r.y1; j < r.y2+1;j++ ){
 		for (int i = r.x1; i < r.x2+1;i++){
 			board[j][i] = num;
+			changes_x.push_back[i];
+			changes_y.push_back[j];
+			changes_id.push_back[num];
 		}
 	}
 
@@ -166,6 +173,9 @@ void fillSet(set<point> res, int num){
 	std::set<point>::iterator it1 = res.begin();
 	for(; it1 != res.end();it1++){
 		board[(*it1).y][(*it1).x] = num;
+		changes_x.push_back[(*it1).x];
+		changes_y.push_back[(*it1).y];
+		changes_id.push_back[num];
 	}
 }
 
@@ -226,6 +236,9 @@ void fillUpInd(int num,int x1,int x2,int y1, int y2){
 	for (int j = y1; j < y2+1;j++ ){
 		for (int i = x1; i < x2+1;i++){
 			board[j][i] = num;
+			changes_x.push_back[i];
+			changes_y.push_back[j];
+			changes_id.push_back[num];
 		}
 	}
 
